a7ec55ce2fe0411f67fb40ea4aa009ab39c15aa9
CHONOU-ORIANE <orianenancy7@gmail.com>
2025-04-26 14:18:55 +0100

    oriane4

--- site_evenement/app/Prestation/component/Hero.tsx
+++ site_evenement/app/Prestation/component/Hero.tsx
@@ -39,7 +39,7 @@ export default function Hero({ title, bgColor, image, breadcrumb }: HeroProps) {
     }, []);
 
     return (
-        <section className={`w-full ${bgColor} dark:bg-gray-900 py-14 pt-0 px-4 md:px-8 lg:px-16`}>
+        <section className={`w-full ${bgColor} dark:bg-gray-900 py-14 px-4 md:px-8 lg:px-16`}>
             <div className="max-w-6xl mx-auto space-y-6 mt-20">
                 <p className="text-amber-700 dark:text-amber-300 uppercase font-semibold tracking-wider animate-item">
                     {breadcrumb}
